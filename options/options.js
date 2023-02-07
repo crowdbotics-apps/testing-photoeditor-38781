01d0e653c99f3069be50a075f5be60357d92d9ec
Crowdbotics <team@crowdbotics.com>
2023-02-07 15:15:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testing-photoeditor-38781",
+  url: "https://testing-photoeditor-38781.botics.co",
+  api: "https://testing-photoeditor-38781.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
